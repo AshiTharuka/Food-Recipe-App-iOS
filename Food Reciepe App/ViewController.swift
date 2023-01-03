723417aae90bae4f35461c3250efc57a257b7b9b
AshiTharuka <Ashihegoda@gmail.com>
2023-01-02 23:28:33 -0800

    Initial Commit

---
+++ Food Reciepe App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Food Reciepe App
+//
+//  Created by Ashi on 1/2/23.
+//  Copyright © 2023 Ashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
